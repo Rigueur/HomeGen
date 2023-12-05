becae8ac999ba3f46ea598bb04697591b189b572
Pierre Karst <pierre.karst@edu.heip.fr>
2023-12-05 17:55:59 +0100

    done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,6 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: %i[flats home]
   def home
     @hidenavbar = true
-  end
-
-  def flats
   end
 end
