0ad0bd8c77c5dc490daf31fe32e63c89b64d8aac
Naomi Doukhan <naomi.doukhan3@gmail.com>
2023-12-06 12:05:53 +0100

    reviews first step by naomi and stewwwww

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,22 @@
+class ReviewsController < ApplicationController
+
+
+  def new
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(params)
+    @review.booking = @booking
+    @review.save
+    redirect_to flats_path # rediriger à l'index des bookings du user
+  end
+
+  private
+
+  def params
+    params.require(:review).permit(:content, :rating)
+  end
+
+end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :flat
   belongs_to :user
+  has_many :reviews
   validates :start_date, :end_date, presence: true
 end
