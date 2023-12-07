75638f6debfef2b82f17ffb19634ae2459a8142b
Steven Despaux <stevendespaux@wanadoo.fr>
2023-12-07 17:06:03 +0100

    homepage done missing buttons

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
+    redirect_to flats_path if current_user
     @hidenavbar = true
   end
 end
