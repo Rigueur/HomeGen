691769ff1d15d86b46d5e776267bcc4765d099a3
Naomi Doukhan <naomi.doukhan3@gmail.com>
2023-12-07 15:54:25 +0100

    Corrigé

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,7 +1,7 @@
 class Flat < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   validates :name, :location, :description, :price_per_month, :autonomy_level, presence: true
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
