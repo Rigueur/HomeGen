b05957bd312e8eebceba716a0f915c49ea693f0c
Pierre Karst <pierre.karst@edu.heip.fr>
2023-12-07 01:05:52 +0100

    flats grid changement de design

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,9 @@
 #   end
 
 # cleaning the database
+
+require 'faker'
+
 puts "Cleaning the database..."
 Favorite.destroy_all
 Review.destroy_all
@@ -22,17 +25,17 @@ User.new(email: "pipicaca@exemple.com", password: "pipicaca", first_name: "Pipi"
 
 
 puts "Creating flats..."
-Flat.new(name: "Chambre agréable en plein Paris", location: "Paris", description: "Nice flat", price_per_month: 500, autonomy_level: 3, user_id: User.last.id, pictures: "https://www.immojeunepro.com/bundles/boresidence/uploads/documents/760x475/98b28a71f210e42e76673dd4029a76c098380d13-760x475.jpeg").save!
-Flat.new(name: "Looking for some young company", location: "London", description: "Cozy flat", price_per_month: 400, autonomy_level: 2, user_id: User.last.id, pictures: "https://www.studylease.co.uk/files/residences/residences/images/image_nexity_PO0000062_5ad73d85709e24_PO0000062_90.jpg" ).save
-Flat.new(name: "Room available close to NYU", location: "New York", description: "Spacious flat", price_per_month: 600, autonomy_level: 4, user_id: User.last.id, pictures: "https://www.passman.fr/wp-content/uploads/2023/10/passenergy-residence-etudiante-e1696258374658.jpg").save
-Flat.new(name: "Zimmer für Studenten zu vermieten", location: "Berlin", description: "Modern flat", price_per_month: 400, autonomy_level: 3, user_id: User.last.id, pictures: "https://www.student-factory.com/media/457/download/StudentFactory_Nice_DSF8115-HDR.jpg?v=1").save
-Flat.new(name: "学生向けのレンタルルーム", location: "Tokyo", description: "Compact flat", price_per_month: 300, autonomy_level: 1, user_id: User.last.id, pictures: "https://thumbs.dreamstime.com/z/chambre-%C3%A0-coucher-de-style-de-l-%C3%A9tudiant-simple-de-dortoir-avec-un-bon-nombre-de-lumi%C3%A8re-79798617.jpg").save
-Flat.new(name: "My elderly mother looking for a student", location: "Sydney", description: "Sunny flat", price_per_month: 350, autonomy_level: 2, user_id: User.last.id, pictures: "https://amazingarchitecture.com/storage/1790/student_room.jpg").save
-Flat.new(name: "stanza in affitto per studenti LUISS", location: "Rome", description: "Historic flat", price_per_month: 300, autonomy_level: 3, user_id: User.last.id, pictures: "https://www.new.ox.ac.uk/sites/default/files/inline-images/Bedroom_0.JPG").save
-Flat.new(name: "se alquila habitacion", location: "Madrid", description: "Central flat", price_per_month: 600, autonomy_level: 4, user_id: User.last.id, pictures: "https://www.nottingham.ac.uk/accommodation/images-multimedia/new-test/room-3.jpg").save
-Flat.new(name: "Amazing clean room next to McGill UNI", location: "Toronto", description: "Suburban flat", price_per_month: 250, autonomy_level: 1, user_id: User.last.id, pictures: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/e800a328914065.55d8b7ced5d02.jpg").save
-Flat.new(name: "Room available", location: "Dubai", description: "Luxury flat", price_per_month: 800, autonomy_level: 5, user_id: User.last.id, pictures: "https://www.cityblock.co.uk/blog/wp-content/uploads/2022/11/34-1024x684.jpg").save
-Flat.new(name: "Looking for a calm student", location: "Singapore", description: "High-rise flat", price_per_month: 400, autonomy_level: 3, user_id: User.last.id, pictures: "https://www.vitastudent.com/wp-content/uploads/2023/01/VITASTUDENT_PROFILES_NOV2021_130-scaled.jpg").save
+Flat.new(name: "Chambre agréable en plein Paris", location: "Paris", description: Faker::Lorem.paragraph(sentence_count: 7, supplemental: true, random_sentences_to_add: 3), price_per_month: 500, autonomy_level: 3, user_id: User.last.id, pictures: "https://www.immojeunepro.com/bundles/boresidence/uploads/documents/760x475/98b28a71f210e42e76673dd4029a76c098380d13-760x475.jpeg").save!
+Flat.new(name: "Looking for some young company", location: "London", description: Faker::Lorem.paragraph(sentence_count: 7, supplemental: true, random_sentences_to_add: 3), price_per_month: 400, autonomy_level: 2, user_id: User.last.id, pictures: "https://www.studylease.co.uk/files/residences/residences/images/image_nexity_PO0000062_5ad73d85709e24_PO0000062_90.jpg" ).save
+Flat.new(name: "Room available close to NYU", location: "New York", description: Faker::Lorem.paragraph(sentence_count: 7, supplemental: true, random_sentences_to_add: 3), price_per_month: 600, autonomy_level: 4, user_id: User.last.id, pictures: "https://www.passman.fr/wp-content/uploads/2023/10/passenergy-residence-etudiante-e1696258374658.jpg").save
+Flat.new(name: "Zimmer für Studenten zu vermieten", location: "Berlin", description: Faker::Lorem.paragraph(sentence_count: 7, supplemental: true, random_sentences_to_add: 3), price_per_month: 400, autonomy_level: 3, user_id: User.last.id, pictures: "https://www.student-factory.com/media/457/download/StudentFactory_Nice_DSF8115-HDR.jpg?v=1").save
+Flat.new(name: "学生向けのレンタルルーム", location: "Tokyo", description: Faker::Lorem.paragraph(sentence_count: 7, supplemental: true, random_sentences_to_add: 3), price_per_month: 300, autonomy_level: 1, user_id: User.last.id, pictures: "https://thumbs.dreamstime.com/z/chambre-%C3%A0-coucher-de-style-de-l-%C3%A9tudiant-simple-de-dortoir-avec-un-bon-nombre-de-lumi%C3%A8re-79798617.jpg").save
+Flat.new(name: "My elderly mother looking for a student", location: "Sydney", description: Faker::Lorem.paragraph(sentence_count: 7, supplemental: true, random_sentences_to_add: 3), price_per_month: 350, autonomy_level: 2, user_id: User.last.id, pictures: "https://amazingarchitecture.com/storage/1790/student_room.jpg").save
+Flat.new(name: "stanza in affitto per studenti LUISS", location: "Rome", description: Faker::Lorem.paragraph(sentence_count: 7, supplemental: true, random_sentences_to_add: 3), price_per_month: 300, autonomy_level: 3, user_id: User.last.id, pictures: "https://www.new.ox.ac.uk/sites/default/files/inline-images/Bedroom_0.JPG").save
+Flat.new(name: "se alquila habitacion", location: "Madrid", description: Faker::Lorem.paragraph(sentence_count: 7, supplemental: true, random_sentences_to_add: 3), price_per_month: 600, autonomy_level: 4, user_id: User.last.id, pictures: "https://www.nottingham.ac.uk/accommodation/images-multimedia/new-test/room-3.jpg").save
+Flat.new(name: "Amazing clean room next to McGill UNI", location: "Toronto", description: Faker::Lorem.paragraph(sentence_count: 7, supplemental: true, random_sentences_to_add: 3), price_per_month: 250, autonomy_level: 1, user_id: User.last.id, pictures: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/e800a328914065.55d8b7ced5d02.jpg").save
+Flat.new(name: "Room available", location: "Dubai", description: Faker::Lorem.paragraph(sentence_count: 7, supplemental: true, random_sentences_to_add: 3), price_per_month: 800, autonomy_level: 5, user_id: User.last.id, pictures: "https://www.cityblock.co.uk/blog/wp-content/uploads/2022/11/34-1024x684.jpg").save
+Flat.new(name: "Looking for a calm student", location: "Singapore", description: Faker::Lorem.paragraph(sentence_count: 7, supplemental: true, random_sentences_to_add: 3), price_per_month: 400, autonomy_level: 3, user_id: User.last.id, pictures: "https://www.vitastudent.com/wp-content/uploads/2023/01/VITASTUDENT_PROFILES_NOV2021_130-scaled.jpg").save
 
 puts "Creating bookings..."
 Booking.new(start_date: "2021-01-01", end_date: "2021-01-31", flat_id: Flat.second.id, user_id: User.last.id).save!
