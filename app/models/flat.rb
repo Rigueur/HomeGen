6dd6f4397ee5bb9f9b095b3544a328c10d5c121f
Steven Despaux <stevendespaux@wanadoo.fr>
2023-12-07 15:55:20 +0100

    index fixed, map interaction partially done

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -5,4 +5,10 @@ class Flat < ApplicationRecord
   validates :name, :location, :description, :price_per_month, :autonomy_level, presence: true
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
+  reverse_geocoded_by :latitude, :longitude do |obj,results|
+    if geo = results.first
+      obj.city    = geo.city
+    end
+  end
+  after_validation :reverse_geocode, if: :will_save_change_to_location?
 end
