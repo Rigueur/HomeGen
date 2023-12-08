0d97e1ce8e1e0ec757e2b0ec87c631b5b367889d
Steven Despaux <stevendespaux@wanadoo.fr>
2023-12-08 12:23:58 +0100

    fixed avatar just in case

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,21 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, :age, presence: true
   after_validation :old?
+  after_validation :avatar
 
   def old?
     if age > 40
       self.old = true
     else
       self.old = false
+    end
+  end
+
+  def avatar
+    if avatar_url.present?
+      self.avatar_url = avatar_url
+    else
+      self.avatar_url = "https://i.imgur.com/lID0B5t.png"
     end
   end
 
